b41f58e54b820533fc87de0d7f26ab60d8945d64
JavadFazelpour <Javad_Fazelpour@outlook.com>
2024-05-20 23:56:18 +0330

    Change Main method to return all guitars

--- FindGuitar/Program.cs
+++ FindGuitar/Program.cs
@@ -10,24 +10,29 @@ internal class Program
         InitializeInventory(inventory);
 
         Guitar whatErinLikes = new Guitar("", 0, Builder.Fender, "Stratocastor", Type.Electric, Wood.Alder, Wood.Alder);
-        Guitar guitar = inventory.Search(whatErinLikes);
-        if (guitar != null)
+        List<Guitar> guitars = inventory.Search(whatErinLikes);
+        if (guitars != null)
         {
-            Console.WriteLine("Erin, you might like these guitars:");
-            StringBuilder sb = new StringBuilder();
-            sb.Append(guitar.Builder);
-            sb.Append(" ");
-            sb.Append(guitar.Model);
-            sb.Append(" ");
-            sb.Append(guitar.Type);
-            sb.Append(" guitar:\n");
-            sb.Append(guitar.BackWood);
-            sb.Append(" back and sides, \n");
-            sb.Append(guitar.TopWood);
-            sb.Append(" top.\nYou can have it for only $");
-            sb.Append(guitar.Price);
-            sb.Append("!");
-            Console.WriteLine(sb);
+            Console.WriteLine("Erin, you might like these guitars:\n");
+
+            foreach (Guitar guitar in guitars)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("We have a ");
+                sb.Append(guitar.Builder);
+                sb.Append(" ");
+                sb.Append(guitar.Model);
+                sb.Append(" ");
+                sb.Append(guitar.Type);
+                sb.Append(" guitar:\n");
+                sb.Append(guitar.BackWood);
+                sb.Append(" back and sides, \n");
+                sb.Append(guitar.TopWood);
+                sb.Append(" top.\nYou can have it for only $");
+                sb.Append(guitar.Price);
+                sb.Append("!\n---");
+                Console.WriteLine(sb); 
+            }
         }
         else
             Console.WriteLine("Sorry, Erin, we have nothing for you.");
@@ -36,5 +41,6 @@ internal class Program
     private static void InitializeInventory(Inventory inventory)
     {
         inventory.AddGuitar("V95693", 1499.95m, Builder.Fender, "Stratocastor", Type.Electric, Wood.Alder, Wood.Alder);
+        inventory.AddGuitar("V9512", 1549.95m, Builder.Fender, "Stratocastor", Type.Electric, Wood.Alder, Wood.Alder);
     }
 }
