199dced0c218243ee7e916447cf5e2e80ff06b46
JavadFazelpour <Javad_Fazelpour@outlook.com>
2024-05-20 23:19:25 +0330

    Fix bug in The Search method of inventory

--- FindGuitar/Inventory.cs
+++ FindGuitar/Inventory.cs
@@ -9,9 +9,12 @@ internal class Inventory
         guitars = new List<Guitar>();
     }
 
-    public void AddGuitar(string serialNumber, decimal price, string builder, string model, string type, string backWood, string topWood)
+    public void AddGuitar(string serialNumber, decimal price,
+                          string builder, string model,
+                          string type, string backWood, string topWood)
     {
-        Guitar guitar = new Guitar(serialNumber, price, builder, model, type, backWood, topWood);
+        Guitar guitar = new Guitar(serialNumber, price, builder,
+                                   model, type, backWood, topWood);
         guitars.Add(guitar);
     }
 
@@ -31,25 +34,27 @@ internal class Inventory
     {
         foreach (var guitar in guitars)
         {
-            string builder = searchGuitar.Builder;
-            if (!string.IsNullOrWhiteSpace(builder) && builder != guitar.Builder)
+            string builder = searchGuitar.Builder.ToLower();
+            if (!string.IsNullOrWhiteSpace(builder) && builder!= guitar.Builder.ToLower())
                 continue;
 
-            string model = searchGuitar.Model;
-            if (!string.IsNullOrWhiteSpace(model) && model != guitar.Model)
+            string model = searchGuitar.Model.ToLower();
+            if (!string.IsNullOrWhiteSpace(model) && model != guitar.Model.ToLower())
                 continue;
 
-            string type = searchGuitar.Type;
-            if (!string.IsNullOrWhiteSpace(type) && type != guitar.Type)
+            string type = searchGuitar.Type.ToLower();
+            if (!string.IsNullOrWhiteSpace(type) && type != guitar.Type.ToLower())
                 continue;
 
-            string backWood = searchGuitar.BackWood;
-            if (!string.IsNullOrWhiteSpace(backWood) && backWood != guitar.BackWood)
+            string backWood = searchGuitar.BackWood.ToLower();
+            if (!string.IsNullOrWhiteSpace(backWood) && backWood != guitar.BackWood.ToLower())
                 continue;
 
-            string topWood = searchGuitar.TopWood;
-            if (!string.IsNullOrWhiteSpace(topWood) && topWood != guitar.TopWood)
+            string topWood = searchGuitar.TopWood.ToLower();
+            if (!string.IsNullOrWhiteSpace(topWood) && topWood != guitar.TopWood.ToLower())
                 continue;
+
+            return guitar;
         }
         return null;
     }
--- FindGuitar/Program.cs
+++ FindGuitar/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.Text;
+
 namespace FindGuitar;
 
 internal class Program
@@ -12,7 +13,21 @@ internal class Program
         Guitar guitar = inventory.Search(whatErinLikes);
         if (guitar != null)
         {
-            Console.WriteLine(guitar.Builder);
+            Console.WriteLine("Erin, you might like these guitars:");
+            StringBuilder sb = new StringBuilder();
+            sb.Append(guitar.Builder);
+            sb.Append(" ");
+            sb.Append(guitar.Model);
+            sb.Append(" ");
+            sb.Append(guitar.Type);
+            sb.Append(" guitar:\n");
+            sb.Append(guitar.BackWood);
+            sb.Append(" back and sides, \n");
+            sb.Append(guitar.TopWood);
+            sb.Append(" top.\nYou can have it for only $");
+            sb.Append(guitar.Price);
+            sb.Append("!");
+            Console.WriteLine(sb);
         }
         else
             Console.WriteLine("Sorry, Erin, we have nothing for you.");
